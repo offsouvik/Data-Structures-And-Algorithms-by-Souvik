6ed79643d8e7bcd436b07720630d43f72c40da1d
offsouvik <offsouvikyt@gmail.com>
2026-05-07 19:28:27 +0530

    added count and merge sort

---
+++ DSA in C language/All about Sorting/Merge-sort.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+
+void printArray(int *A, int n){
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", A[i]);
+    }
+
+    printf("\n");
+}
+
+void merge(int A[], int mid, int low, int high){
+
+    int i, j, k;
+    int B[100];
+
+    i = low;
+    j = mid + 1;
+    k = low;
+
+    while(i <= mid && j <= high){
+
+        if(A[i] < A[j]){
+
+            B[k] = A[i];
+
+            i++;
+            k++;
+        }
+        else{
+
+            B[k] = A[j];
+
+            j++;
+            k++;
+        }
+    }
+
+    while(i <= mid){
+
+        B[k] = A[i];
+
+        k++;
+        i++;
+    }
+
+    while(j <= high){
+
+        B[k] = A[j];
+
+        k++;
+        j++;
+    }
+
+    for(int i = low; i <= high; i++){
+        A[i] = B[i];
+    }
+}
+
+void mergeSort(int A[], int low, int high){
+
+    int mid;
+
+    if(low < high){
+
+        mid = (low + high) / 2;
+
+        printf("\nDividing Array from %d to %d\n", low, high);
+
+        mergeSort(A, low, mid);
+
+        mergeSort(A, mid + 1, high);
+
+        printf("Merging from %d to %d\n", low, high);
+
+        merge(A, mid, low, high);
+
+        printArray(A, 7);
+    }
+}
+
+int main(){
+
+    int A[] = {9, 1, 4, 14, 4, 15, 6};
+
+    int n = 7;
+
+    printf("Before Sorting:\n");
+    printArray(A, n);
+
+    mergeSort(A, 0, n - 1);
+
+    printf("\nAfter Sorting:\n");
+    printArray(A, n);
+
+    return 0;
+}
